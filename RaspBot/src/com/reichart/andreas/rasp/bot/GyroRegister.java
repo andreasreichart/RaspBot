3d03e346a8d435f57bbade6554b96ede2f61c9a2
Andreas Reichart <andreas.reichart@gmail.com>
2013-12-14 21:27:36 +0100

    Added gyro-registers to the GyroRegister enum.
    Change-Id: Ibd09eeb7a60bac5e54e8e2b4789bad9129599a4a

--- RaspBot/src/com/reichart/andreas/rasp/bot/GyroRegister.java
+++ RaspBot/src/com/reichart/andreas/rasp/bot/GyroRegister.java
@@ -1,18 +1,16 @@
 package com.reichart.andreas.rasp.bot;
 
-import java.util.HashMap;
-
 public enum GyroRegister {
 
-    PWR_MGMT_1(0x6B), 
-    PWR_MGMT_2(0x6C);
+    PWR_MGMT_1(0x6B), PWR_MGMT_2(0x6C), GYRO_CONFIG(0x1B), GYRO_SELFTEST_X(0x0D), GYRO_SELF_TEST_Y(0x0E), GYRO_SELF_TEST_Z(
+	    0x0F), GYRO_SELF_TEST_ACCEL (0x10);
 
     final int register;
-    
+
     GyroRegister(int register) {
 	this.register = register;
     }
-    
+
     public int getValue() {
 	return register;
     }
